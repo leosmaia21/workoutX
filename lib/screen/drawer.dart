c7a28ac000f49464963a845bb4dd175deaf72a37
leosmaia21 <leosmaia21@gmail.com>
2021-09-27 03:55:36 +0100

    drawer ja com messagem em baixo

--- lib/screen/drawer.dart
+++ lib/screen/drawer.dart
@@ -11,51 +11,78 @@ class NavigationDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-        child: Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: const [
-            Color(0xFFF77723),
-            Color(0xFFF7A456),
-            Color(0xFFF7BE8A),
-            Color(0xFFF5EEE9),
-          ],
-        ),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: const [
+                    Color(0xFFF77723),
+                    Color(0xFFF7A456),
+                    Color(0xFFF7BE8A),
+                    Color(0xFFF5EEE9),
+                  ],
+                ),
+              ),
+              child: ConstrainedBox(
+                constraints: constraints.copyWith(
+                  minHeight: constraints.maxHeight,
+                  maxHeight: double.infinity,
+                ),
+                child: IntrinsicHeight(
+                  child: SafeArea(
+                    child: Column(
+                      children: [
+                        Column(
+                          children: <Widget>[
+                            SizedBox(
+                              height: 50,
+                            ),
+                            buildMenuItem("text", Icons.fitness_center, () {
+                              null;
+                            }),
+                          ],
+                        ),
+                        Expanded(
+                          child: Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [Text('ola'),Text('hola'),]
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
       ),
-      child: Material(
-        color: Colors.transparent,
-        child: ListView(
-          children: [
-            SizedBox(height: 50),
-            buildMenuItem('Treinos antigos', Icons.fitness_center,()=>null),
-             SizedBox(height: 10),
-            buildMenuItem('Medições', Measure.ruler,()=>null),
-             Container(alignment: Alignment.bottomCenter,child: Text('ola'),)
-          ],
-        ),
-      ),
-    ));
+    );
   }
 
   Widget buildMenuItem(String text, IconData icon, VoidCallback? onClicked) {
     return ListTile(
-      leading: Icon(icon, color: Colors.white),
-      title: Text(
-        text,
-        style: TextStyle(
-          color: Colors.white,
-          shadows: <Shadow>[
-            Shadow(
-              //offset: Offset(10.0, 10.0),
-              blurRadius: 20.0,
-              color: Colors.black,
-            ),
-          ],
+        leading: Icon(icon, color: Colors.white),
+        title: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            shadows: <Shadow>[
+              Shadow(
+                //offset: Offset(10.0, 10.0),
+                blurRadius: 20.0,
+                color: Colors.black,
+              ),
+            ],
+          ),
         ),
-      ),
-      onTap: onClicked
-    );
+        onTap: onClicked);
   }
 }
