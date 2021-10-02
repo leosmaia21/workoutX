68e3ebb5ff6a7bd185f647d4daed7319cd13744e
leonardomaia <leonardomaia@ua.pt>
2021-10-02 23:21:28 +0100

    homescreen ja com os treinos aparecer

--- lib/databasemanager/databasemanager.dart
+++ lib/databasemanager/databasemanager.dart
@@ -7,6 +7,7 @@ import 'package:flutter/cupertino.dart';
 
 class DatabaseManager {
   final FirebaseStorage _storage = FirebaseStorage.instance;
+  final _firestore = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
 
   CollectionReference users = FirebaseFirestore.instance.collection('users');
@@ -16,17 +17,17 @@ class DatabaseManager {
   File? imageFile; // fotografia do drawer
   String? urlImage;
   var image;
-  bool hasPhoto=false;
+  bool hasPhoto = false;
   //String? imagePath;
 
   Future<String?> getProfileImage() async {
     String uid = _auth.currentUser!.uid.toString();
     try {
-     String url= await _storage.ref('users_photos/' + uid).getDownloadURL();
-     urlImage=url;
-     image= NetworkImage(url);
-     hasPhoto=true;
-     return url;
+      String url = await _storage.ref('users_photos/' + uid).getDownloadURL();
+      urlImage = url;
+      image = NetworkImage(url);
+      hasPhoto = true;
+      return url;
     } catch (e) {
       print('foto NAO buscada na storage');
       print(e.toString());
@@ -34,8 +35,8 @@ class DatabaseManager {
     }
   }
 
-  Future init() async{
- User? user = _auth.currentUser;
+  Future init() async {
+    User? user = _auth.currentUser;
 
     _userUID = user!.uid.toString();
     print('Utilizador!!!!  ' + _userUID!);
@@ -44,20 +45,32 @@ class DatabaseManager {
     print(data['name']);
     _userName = data['name'];
 
-     String uid = _auth.currentUser!.uid.toString();
+    String uid = _auth.currentUser!.uid.toString();
     try {
-     String url= await _storage.ref('users_photos/' + uid).getDownloadURL();
-     urlImage=url;
-     image= NetworkImage(url);
-     hasPhoto=true;
-   
+      String url = await _storage.ref('users_photos/' + uid).getDownloadURL();
+      urlImage = url;
+      image = NetworkImage(url);
+      hasPhoto = true;
     } catch (e) {
       print('foto NAO buscada na storage');
       print(e.toString());
-   
     }
   }
 
+  Future<Map> getWorkouts() async {
+    String uid = _auth.currentUser!.uid.toString();
+    try {
+      DocumentSnapshot x =
+          await _firestore.collection('workouts').doc(uid).get();
+      var xx = x.data() as Map;
+      print('treinos:');
+      print(xx);
+      return xx;
+    } catch (e) {
+      print('não há treinos');
+      return {};
+    }
+  }
 
   Future<void> insertUser(
       {required String name,
@@ -86,12 +99,12 @@ class DatabaseManager {
     return _userName;
   }
 
-  void setLogOut(){
-   _userName= null;
-   _userUID=null;
-   imageFile=null; // fotografia do drawer
-   urlImage=null;
-   image=null;
-   hasPhoto=false;
+  void setLogOut() {
+    _userName = null;
+    _userUID = null;
+    imageFile = null; // fotografia do drawer
+    urlImage = null;
+    image = null;
+    hasPhoto = false;
   }
 }
--- lib/screen/homescreen.dart
+++ lib/screen/homescreen.dart
@@ -1,9 +1,12 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:workout/databasemanager/databasemanager.dart';
 import 'package:workout/screen/workout.dart';
 import 'package:workout/services/authservice.dart';
+import 'package:workout/utilities/toast.dart';
 import 'drawer.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -14,16 +17,16 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
- 
+  late Future<Map> _workouts;
+  Map? x;
   @override
-  void initState()  {
+  initState() {
     super.initState();
-   
+    _workouts = context.read<DatabaseManager>().getWorkouts();
   }
-  
+
   String? userName;
- 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,27 +45,82 @@ class _HomeScreenState extends State<HomeScreen> {
                   color: Colors.black,
                 ),
                 onPressed: () async {
-                   
                   Scaffold.of(context).openDrawer();
-                 
                 },
               );
             });
           },
         ),
       ),
-      body: SizedBox(
-        height: double.infinity,
-        width: double.infinity,
-        child: Center(
-          child: SizedBox(
-            height: 50,
-            width: 50,
-            child: TextButton(
-              child: Text('go workout'),
-              onPressed: (){
-                   Navigator.push(context,MaterialPageRoute(builder: (context) => Workout()));
-              },
+      body: FutureBuilder<Object>(
+        future: _workouts,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            x = snapshot.data as Map;
+            return Column(
+              children: [
+                SizedBox(height: MediaQuery.of(context).size.height * 0.15),
+                for (int i = 1; i <= x!.length; i++)
+                  tile(Icons.ac_unit_outlined, x!['$i'], 10),
+              ],
+            );
+          } else {
+            return const Center(
+              child: Image(
+                image: AssetImage('lib/assets/images/cara_moniz_gif1.gif'),
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  Widget tile(IconData icon, String name, int number) {
+    final border =
+        RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0));
+    return Container(
+      // alignment: Alignment.center,
+      // width: MediaQuery.of(context).size.width,
+      height: 100,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          // vertical: 20,
+          horizontal: 20,
+        ),
+        child: InkWell(
+          
+          onTap: () => toast('ole'),
+          child: Card(
+            // margin: EdgeInsets.zero,
+            // clipBehavior: Clip.values,
+            shape: border,
+            color: Colors.black45,
+            elevation: 7,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    SizedBox(width: 20),
+                    Icon(icon),
+                    SizedBox(width: 20),
+                    Text(
+                      name,
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+                // SizedBox(width: MediaQuery.of(context).size.width * 0.2),
+                Row(
+                  children: [
+                    Text('$number'),
+                    SizedBox(width: 30),
+                  ],
+                ),
+                // SizedBox(width: 20),
+              ],
             ),
           ),
         ),
